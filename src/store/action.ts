acb71fac44084fa6ee0a8509b728692d64382cc5
leunamme4 <90042349+leunamme4@users.noreply.github.com>
2024-05-27 19:51:49 +0300

    module task 7-1

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,9 +1,17 @@
 import {createAction} from '@reduxjs/toolkit';
+import { Offer } from '../types/offer';
+import { AuthorizationStatus } from '../consts/autorization-status';
 
 export const cityChange = createAction<string>('сityChange');
-
-export const listFilling = createAction('listFilling');
 
 export const sortTypeSelect = createAction<string>('sortTypeSelect');
 
 export const highlightMarker = createAction<{ id: string } | null>('highlightMarker');
+
+export const loadOffers = createAction<Offer[]>('loadOffers');
+
+export const setOffersDataLoadingStatus = createAction<boolean>('setOffersDataLoadingStatus');
+
+export const setError = createAction<string | null>('setError');
+
+export const requireAuthorization = createAction<AuthorizationStatus>('requireAuthorization');
